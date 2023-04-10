4d06b7bef117de40db44502f2941e37677d36d58
rey <114239691+indigowitchh@users.noreply.github.com>
2023-04-10 09:56:30 -0600

    Update sheepcatcher.py

--- sheepcatcher.py
+++ sheepcatcher.py
@@ -1,4 +1,3 @@
-import pygame
 import random
 
 #set up pygame stuff
@@ -13,11 +12,12 @@ timer = 0 #used for sheep movement
 score = 0
 
 #images and fonts
-SheepPic = pygame.image.load("sheep.jpg")
+SheepPic = pygame.image.load("C:/Users/793767/Desktop/sheep/sheep.jpg")
+PlayerPic = pygame.image.load("C:/Users/793767/Desktop/sheep/player.jpg")
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render('Score:', True, (200, 200, 0))
 text2 = font.render(str(score), True, (200, 200, 0))
-text3 = font.render('YOU WIN!', True, (200, 200, 0))
+text3 = font.render('YOU KICKED CARTMANS BUTT!', True, (200, 200, 0))
 
 #CONSTANTS (not required, just makes code easier to read)
 LEFT = 0
@@ -53,8 +53,8 @@ def sheepMove(position):
     return position
 
 def collision(PlayerX, PlayerY, sheepInfo):
-    if PlayerX+40 > sheepInfo[0]:
-       if PlayerX < sheepInfo[0]+40:
+    if PlayerX+40 > sheepInfo[0]: #check right edge of player against left edge of animal
+       if PlayerX < sheepInfo[0]+80: #checking left edge of player against right edge of animal
            if PlayerY+40 >sheepInfo[1]:
                if PlayerY < sheepInfo[1]+40:
                    if sheepInfo[3] == False: #only catch uncaught sheeps!
@@ -68,10 +68,10 @@ def collision(PlayerX, PlayerY, sheepInfo):
 #numbers in list represent xpos, ypos, direction moving, and whether it's been caught or not!
 sheep1 = [200, 400, RIGHT, False]
 sheep2 = [300, 500, LEFT, False]
-sheep3 = [700, 500, LEFT, False]
-sheep4 = [350, 550, LEFT, False]
-sheep5 = [470, 200, LEFT, False]
-sheep6 = [100, 500, LEFT, False]
+sheep3 = [400, 500, LEFT, False]
+sheep4 = [350, 550, RIGHT, False]
+sheep5 = [770, 200, LEFT, False]
+sheep6 = [600, 500, RIGHT, False]
 #make more sheeps here!
 
 
@@ -85,7 +85,7 @@ keys = [False, False, False, False] #this list holds whether each key has been p
 
 
 
-while score<1: #GAME LOOP############################################################
+while score<6: #GAME LOOP############################################################
     clock.tick(60) #FPS
     timer+=1
     
@@ -138,27 +138,51 @@ while score<1: #GAME LOOP#######################################################
 
     #player/sheep collision!
     collision(xpos, ypos, sheep1)
-
+    collision(xpos, ypos, sheep2)
+    collision(xpos, ypos, sheep3)
+    collision(xpos, ypos, sheep4)
+    collision(xpos, ypos, sheep5)
+    collision(xpos, ypos, sheep6)
+    
+    
     #update player position
     xpos+=vx 
     ypos+=vy
     
     #update sheep position
     sheep1 = sheepMove(sheep1)
-  
+    sheep2 = sheepMove(sheep2)
+    sheep3 = sheepMove(sheep3)
+    sheep4 = sheepMove(sheep4)
+    sheep5 = sheepMove(sheep5)
+    sheep6 = sheepMove(sheep6)
+    
     # RENDER
     # Section--------------------------------------------------------------------------------
             
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
   
     #draw player
-    pygame.draw.rect(screen, (100, 200, 100), (xpos, ypos, 40, 40))
-
+    screen.blit(PlayerPic, (xpos,ypos,40,40))
     #draw sheep
     if sheep1[3] == False: #don't draw them if they're already caught!
-        #pygame.draw.rect(screen, (250, 100, 100), (sheep1[0], sheep1[1], 40, 40)) #use this if you don't have a jpg
         screen.blit(SheepPic, (sheep1[0], sheep1[1]))
     
+    if sheep2[3] == False:
+        screen.blit(SheepPic, (sheep2[0], sheep2[1]))
+        
+    if sheep3[3] == False: #don't draw them if they're already caught!
+        screen.blit(SheepPic, (sheep3[0], sheep3[1]))
+    
+    if sheep4[3] == False:
+        screen.blit(SheepPic, (sheep4[0], sheep4[1]))
+
+    if sheep5[3] == False: #don't draw them if they're already caught!
+        screen.blit(SheepPic, (sheep5[0], sheep5[1]))
+    
+    if sheep6[3] == False:
+        screen.blit(SheepPic, (sheep6[0], sheep6[1]))
+        
     #display score
     screen.blit(text, (20, 20))
     text2 = font.render(str(score), True, (200, 200, 0))#update score number
@@ -170,7 +194,7 @@ while score<1: #GAME LOOP#######################################################
 
 #end screen
 screen.fill((0,0,0)) 
-screen.blit(text3, (400,400))
+screen.blit(text3, (100,100))
 pygame.display.flip()
 pygame.time.wait(2000)#pause for a bit before ending
 
